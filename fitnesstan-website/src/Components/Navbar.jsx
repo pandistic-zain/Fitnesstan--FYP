118ee30c58b2dd66e8028c920f5fe064b39fa0a6
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-09-21 02:40:27 -0700

    "Added Bootstrap dependencies and updated Navbar component to use react-bootstrap"

--- fitnesstan-website/src/Components/Navbar.jsx
+++ fitnesstan-website/src/Components/Navbar.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // Importing custom CSS
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/features">Features</Link></li>
-      </ul>
-    </nav>
+    <Navbar bg="light" variant="light" expand="lg" className="custom-navbar" sticky="top">
+      <Container>
+        {/* Logo/Brand */}
+        <Navbar.Brand as={Link} to="/">
+          <img 
+            src="/path-to-your-logo/logo.png" 
+            alt="Fitnesstan Logo" 
+            className="navbar-logo"
+          />
+          Fitnesstan
+        </Navbar.Brand>
+
+        {/* Collapsible Menu */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Links with Pakistani Flag Colors */}
+          <Nav className="ml-auto nav-links">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/features">
+              Features
+            </Nav.Link>
+            <Nav.Link as={Link} to="/supplements">
+              Supplements
+            </Nav.Link>
+
+            {/* Login/Sign Up buttons */}
+            <Button as={Link} to="/login" className="mx-2" variant="outline-primary">
+              Login
+            </Button>
+            <Button as={Link} to="/signup" variant="primary">
+              Sign Up
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavBar;
