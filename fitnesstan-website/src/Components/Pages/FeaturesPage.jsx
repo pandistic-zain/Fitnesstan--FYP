58e8beb719cf0bdb73f5ded27d5345bb762863f4
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-09-21 02:23:30 -0700

    "Added react-router-dom, updated package.json and package-lock.json, removed App.css, and modified App.js and index.js to use react-router-dom"

---
+++ fitnesstan-website/src/Components/Pages/FeaturesPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FeaturesPage = () => {
+  return (
+    <div>
+      <h1>App Features</h1>
+      <ul>
+        <li>Tailorized Diet Plans</li>
+        <li>Personalized Workout Plans</li>
+        <li>Diabetes Support</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FeaturesPage;
---
+++ fitnesstan-website/src/Components/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Fitnesstan</h1>
+      <p>Your personalized fitness journey begins here!</p>
+      <button>Learn More</button>
+    </div>
+  );
+};
+
+export default HomePage;
