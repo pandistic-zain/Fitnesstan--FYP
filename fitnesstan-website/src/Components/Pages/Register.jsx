c48ba3448ac14922da6d83120b905a2ad10cb2fc
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-09-27 06:06:42 -0700

    "Updated Register page CSS and JSX: changed background image, styles, and layout; added Go Back button; modified form containers and overlay panels."

--- fitnesstan-website/src/Components/Pages/Register.jsx
+++ fitnesstan-website/src/Components/Pages/Register.jsx
@@ -1,6 +1,5 @@
-
-import React, { useState } from 'react';
-import './Register.css';// You can rename the CSS file if you'd like
+import React, { useState } from "react";
+import "./Register.css"; // You can rename the CSS file if you'd like
 
 const Register = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -11,54 +10,100 @@ const Register = () => {
 
   return (
     <>
-    <div className="register-page">
-    <div className={`container ${isSignUp ? 'right-panel-active' : ''}`} id="container">
-      <div className="form-container sign-up-container">
-        <form action="signup">
-          <h1>SIGN UP</h1>
-          <div className="social-container">
+      <div className="register-page">
+        <div className="GoBack">
+          <button className="go-back-button" type="button">
+            <div className="button-background">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1024 1024"
+                height="25px"
+                width="25px"
+              >
+                <path
+                  d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+                  fill="#000000"
+                ></path>
+                <path
+                  d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+                  fill="#000000"
+                ></path>
+              </svg>
+            </div>
+            <p className="go-back-text">Go Back</p>
+          </button>
+        </div>
+        <div
+          className={`container ${isSignUp ? "right-panel-active" : ""}`}
+          id="container"
+        >
+          <div className="form-container sign-up-container">
+            <form action="signup">
+              <h1>SIGN UP</h1>
+              {/* <div className="social-container">
             <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
             <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
             <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
           </div>
-          <span>or use your email for registration</span>
-          <input type="text" name="username" placeholder="Name" required />
-          <input type="email" name="email" placeholder="Email" required />
-          <input type="password" name="password" placeholder="Password" required />
-          <button type="submit">Sign Up</button>
-        </form>
-      </div>
-      <div className="form-container sign-in-container">
-        <form action="login">
-          <h1>LOGIN</h1>
-          <div className="social-container">
+          <span>or use your email for registration</span> */}
+              <input type="text" name="username" placeholder="Name" required />
+              <input type="email" name="email" placeholder="Email" required />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              <button type="submit">Sign Up</button>
+            </form>
+          </div>
+          <div className="form-container sign-in-container">
+            <form action="login">
+              <h1>LOGIN</h1>
+              {/* <div className="social-container">
             <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
             <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
             <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
           </div>
-          <span>or use your account</span>
-          <input type="email" name="email" placeholder="Email" required />
-          <input type="password" name="password" placeholder="Password" required />
-          <a href="#">Forgot your password?</a>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h1>Welcome Back!</h1>
-            <p>To keep connected with us please login with your personal info</p>
-            <button className="ghost" onClick={handleToggle}>Login</button>
+          <span>or use your account</span> */}
+              <input type="email" name="email" placeholder="Email" required />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
+              {/* eslint-disable-next-line */}
+              <a href="#">Forgot your password?</a>
+              <button type="submit">Login</button>
+            </form>
           </div>
-          <div className="overlay-panel overlay-right">
-            <h1>Hello, Friend!</h1>
-            <p>Enter your personal details and start your journey with us</p>
-            <button className="ghost" onClick={handleToggle}>Sign Up</button>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Welcome Back to Fitnesstan!</h1>
+                <p>
+                  Log in to access your personalized fitness and nutrition
+                  plans, crafted just for you.
+                </p>
+                <button className="ghost" onClick={handleToggle}>
+                  Login
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1>Hello, Fitness Enthusiast!</h1>
+                <p>
+                  Join Fitnesstan today and embark on a tailored fitness
+                  journey, complete with workout and nutrition guidance.
+                </p>
+                <button className="ghost" onClick={handleToggle}>
+                  Sign Up
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     </>
   );
 };
