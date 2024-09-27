6d9e49a80262a4e504543873cdff0b3884c1b39f
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-09-27 06:22:06 -0700

    "Refactored Register page CSS and JSX: updated styles for GoBack button and rearranged HTML structure"

--- fitnesstan-website/src/Components/Pages/Register.jsx
+++ fitnesstan-website/src/Components/Pages/Register.jsx
@@ -12,26 +12,28 @@ const Register = () => {
     <>
       <div className="register-page">
         <div className="GoBack">
-          <button className="go-back-button" type="button">
-            <div className="button-background">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1024 1024"
-                height="25px"
-                width="25px"
-              >
-                <path
-                  d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
-                  fill="#000000"
-                ></path>
-                <path
-                  d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
-                  fill="#000000"
-                ></path>
-              </svg>
-            </div>
-            <p className="go-back-text">Go Back</p>
-          </button>
+          <div class="scene">
+              <span class="side front">
+                <div className="button-background">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1024 1024"
+                  height="25px"
+                  width="25px"
+                >
+                  <path
+                    d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+                    fill="#000000"
+                  ></path>
+                  <path
+                    d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+                    fill="#000000"
+                  ></path>
+                </svg>
+                </div>
+                Go Back
+              </span>
+          </div>
         </div>
         <div
           className={`container ${isSignUp ? "right-panel-active" : ""}`}
@@ -72,7 +74,7 @@ const Register = () => {
                 name="password"
                 placeholder="Password"
                 required
-              />
+              /> 
               {/* eslint-disable-next-line */}
               <a href="#">Forgot your password?</a>
               <button type="submit">Login</button>
