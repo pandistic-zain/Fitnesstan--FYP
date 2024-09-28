6bb1a74c49cda3af2fcc0a7babde996241face42
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-09-28 08:05:04 +0500

    "Removed Button import from react-bootstrap in FeaturesPage.jsx"

--- fitnesstan-website/src/Components/Pages/FeaturesPage.jsx
+++ fitnesstan-website/src/Components/Pages/FeaturesPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import styles from "./Features.module.css";
 
