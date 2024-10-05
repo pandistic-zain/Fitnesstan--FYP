95bb30d8fb843e6d250c2ce375d66272ab6edcea
Zain Ul Abdeen <zain.alphanetworks@gmail.com>
2024-10-05 17:58:16 +0500

    "Added AdminDashboard component and route to Front component"

--- fitnesstan-website/src/Components/Front.jsx
+++ fitnesstan-website/src/Components/Front.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import FeaturesPage from './Pages/FeaturesPage';
 import SupplementsPage from './Pages/SupplementsPage';
+import AdminDashboard from './AdminPanel/AdminDashboard';
 import Navbar from './CustomNavbar';
 import Footer from './Footer';
 
@@ -14,6 +15,7 @@ function Front() {
         <Route path="/" element={<HomePage />}/>
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/supplements" element={<SupplementsPage />} />
+        <Route path="/AdminDashboard" element={<AdminDashboard />} />
       </Routes>
       <Footer />
     </>
