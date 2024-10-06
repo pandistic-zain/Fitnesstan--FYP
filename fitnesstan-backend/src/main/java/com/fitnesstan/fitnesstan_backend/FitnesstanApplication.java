70679f81856e63f44d73092688e7427b96bdeb0c
Zain Ul Abdeen <pandistic.zain@gmail.com>
2024-10-06 17:31:05 +0500

    initail backend commit

---
+++ fitnesstan-backend/src/main/java/com/fitnesstan/fitnesstan_backend/FitnesstanApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesstan.fitnesstan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesstanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesstanApplication.class, args);
+	}
+
+}
---
+++ fitnesstan-backend/src/test/java/com/fitnesstan/fitnesstan_backend/FitnesstanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesstan.fitnesstan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesstanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
